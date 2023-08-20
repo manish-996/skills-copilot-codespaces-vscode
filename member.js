6432bcddccda6f088ebed45c93b718ebdda1c1e1
manish-996 <54453970+manish-996@users.noreply.github.com>
2023-08-20 21:18:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = document.getElementById("skills").value;
+    var skillsError = document.getElementById("skillsError");
+    if (skills == "") {
+        skillsError.innerHTML = "Skills is required";
+        return false;
+    } else {
+        skillsError.innerHTML = "";
+        return true;
+    }
+}
